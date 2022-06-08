8d18e3b3f5c1bf0916b839774b8d4de1b41c4fb8
Sami Freese <sami.freese@gmail.com>
2022-06-08 17:49:43 -0500

    progress during class

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,27 +1,31 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
-    private String username;
+
+    @NotBlank(message = "Username is required")
+    @Size(min=5, max=15, message = "Username must be between 5 and 15 characters")
+    private String userame;
+
+    @Email(regexp=".+@.+\\..+||null")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=6, message = "Password must be at least six characters")
     private String password;
 
-    public User() {
+    @NotBlank(message = "Please re-enter your password")
+    private String verifyPassword;
 
+    public String getUserame() {
+        return userame;
     }
 
-    public User(String username, String email, String password) {
-        this();
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUserame(String userame) {
+        this.userame = userame;
     }
 
     public String getEmail() {
@@ -38,5 +42,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
     }
 }
